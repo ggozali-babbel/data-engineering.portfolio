0f591aa8cae8af7cd7ad2b2c0f72f3dacee27ae0
ggozali-babbel <ggozali@babbel.com>
2025-10-01 16:00:42 -0400

    setup base

--- crypto-pipeline/data-ingestion/main.py
+++ crypto-pipeline/data-ingestion/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from crypto-pipeline!")
+import os
+from dotenv import load_dotenv
 
+def main():
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Retrieve your API key
+    api_key = os.getenv("COINGECKO_API_KEY")
+
+    if not api_key:
+        raise ValueError("COINGECKO_API_KEY not found..")
+    else:
+        print(f"My API key is: {api_key}")
 
 if __name__ == "__main__":
     main()

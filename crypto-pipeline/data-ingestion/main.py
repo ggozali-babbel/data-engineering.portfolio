7a918dc23f77aa441f306723594dfef32518e487
ggozali-babbel <ggozali@babbel.com>
2025-10-02 17:00:18 -0400

    push code

--- crypto-pipeline/data-ingestion/main.py
+++ crypto-pipeline/data-ingestion/main.py
@@ -1,17 +1,40 @@
 import os
-from dotenv import load_dotenv
+from datetime import datetime
+import pandas as pd
+from pycoingecko import CoinGeckoAPI
 
 def main():
-    # Load environment variables from .env file
-    load_dotenv()
+    # Initialize CoinGecko API client
+    cg = CoinGeckoAPI()
 
-    # Retrieve your API key
-    api_key = os.getenv("COINGECKO_API_KEY")
+    # List of cryptocurrency IDs
+    coin_ids = ['bitcoin', 'ethereum', 'solana', 'dogecoin', 'cardano', 'xrp']
 
-    if not api_key:
-        raise ValueError("COINGECKO_API_KEY not found..")
-    else:
-        print(f"My API key is: {api_key}")
+    # Fetch market data
+    data = cg.get_coins_markets(
+        vs_currency='usd',
+        ids=coin_ids,
+        order='market_cap_desc',
+        per_page=100,
+        page=1,
+        price_change_percentage='24h'
+    )
+
+    # Convert to pandas DataFrame
+    df = pd.DataFrame(data)
+
+    # Select and rename relevant columns for readability
+    selected_columns = [
+        'id', 'symbol', 'name', 'current_price', 'market_cap', 'total_supply',
+        'ath', 'ath_date', 'atl', 'atl_date', 'price_change_percentage_24h'
+    ]
+    df_selected = df[selected_columns]
+
+    # add loaded_date for logging
+    loaded_date = datetime.now().strftime('%Y-%m-%d')
+    df_selected['loaded_date'] = loaded_date
+
+    print(df_selected)
 
 if __name__ == "__main__":
     main()

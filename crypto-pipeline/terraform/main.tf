46773a2002bd62165b41ee5daaf47e162ca37a27
ggozali-babbel <ggozali@babbel.com>
2025-10-04 11:10:56 -0400

    test

--- crypto-pipeline/terraform/main.tf
+++ crypto-pipeline/terraform/main.tf
@@ -6,3 +6,8 @@ resource "aws_s3_bucket" "cryptoData" {
     bucket = "gg-crypto-data"
     acl = "private"
 }
+
+resource "aws_s3_bucket" "testtoDelete" {
+    bucket = "gg-crypto-data22"
+    acl = "private"
+}
